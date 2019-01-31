d0b84da57a8f0541e9f4bd2faf83504ef4bea65c
Michelle Yang <mly007@ucsd.edu>
2019-01-31 10:01:54 -0800

    Using index.js to load in data as opposed to statically.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@
  * GET home page.
  */
 
+
 exports.view = function(req, res){
-  res.render('index');
-};
+  res.render('index', {
+    'name': 'Waiting in Line',
+    'image': 'lorempixel.people.1.jpeg',
+    'id': 'project1'
+  });
+};	
